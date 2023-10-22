cead3ff8d9c6b67465159fef7664247a77b10cec
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2023-10-22 16:13:48 +0200

    finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,13 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const LOCAL_KEY = `videoplayer-current-time`;
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+    player.on('timeupdate', throttle(function({seconds}) {
+        localStorage.setItem(LOCAL_KEY, seconds);
+    }, 1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+        const time = localStorage.getItem(LOCAL_KEY) || 0;
+        player.setCurrentTime(time);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
 
+import throttle from "lodash.throttle";
+
+const formEl = document.querySelector(`.feedback-form`);
+
+formEl.addEventListener('input', onInput);
+formEl.addEventListener('submit', onSubmit);
+window.addEventListener('load', onLoad)
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+function onInput(evt) {
+    formData[evt.target.name] = evt.target.value.trim();
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    console.log(formData);
+    formData = {};
+    evt.target.reset();
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+function onLoad() {
+    
+    try {
+        const data = localStorage.getItem(LOCAL_STORAGE_KEY);
+        formData = JSON.parse(data);
+        Object.entries(formData).forEach(([key, val]) => {
+            const element = formEl.elements[key];
+            if (element) {
+                element.value = val;
+            }
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
